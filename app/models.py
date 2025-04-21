57c107bf377e7bdf7b16aa3ade48271309de96f9
hamzaaldabe <hamza3849@gmail.com>
2025-04-21 21:29:34 +0300

    text

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.Text)
     domains = db.relationship('Domain', backref='owner', lazy=True)
 
     def set_password(self, password):
